7a99a196ae51160e8417ebb6a70c86052d33a458
normal-wls <1158341873@qq.com>
2021-01-12 19:33:36 +0800

    minor: unitest debug

--- gcloud/apigw/utils.py
+++ gcloud/apigw/utils.py
@@ -41,7 +41,7 @@ def api_hash_key(*args, **kwargs):
                 request_params = str(sorted(params.items()))
             else:
                 break
-            request_tag = "path:{},user:{},params:{}".format(request.path, request.user.username, request_params)
+            request_tag = "path:{},params:{}".format(request.path, request_params)
             new_args = args[:idx] + (request_tag,) + args[idx + 1 :]
             break
 
